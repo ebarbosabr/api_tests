4421fb42e4716c97f1336a1e36732fcc413ca496
ebarbosabr <ebarbosa.br@gmail.com>
2023-04-20 10:04:45 +1000

    Class User created

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,19 @@
+package br.com.dicasdeumdev.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
